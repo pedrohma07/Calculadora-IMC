782483c58545ab88a642dea94a337f5d172b3c48
Pedrohma07 <pedrohma07@gmail.com>
2022-09-30 19:16:20 -0300

    Configurando responsividade

--- script.js
+++ script.js
@@ -15,32 +15,38 @@ function imc (){
     }else{
         resultado.value = peso.value / (Math.pow(altura.value, 2));
         if(resultado.value < 18.5){
-            resultado.textContent = `Seu IMC é ${resultado.value.toFixed(2)} e você está abaixo do peso`;
+            resultado.innerHTML = `Seu IMC é ${resultado.value.toFixed(2)} e você está abaixo do peso
+                                    <a href="#ilustracao"><img src="assets/seta.svg" alt="" class="seta"></a>`;
             ilustracao.innerHTML = `<img src="assets/img1.png" alt="" class="ilustracao-item">`;
             ilustracao.style.display = "flex";
         }
         else if(resultado.value >= 18.5 && resultado.value <= 24.99){
-            resultado.textContent = `Seu IMC é ${resultado.value.toFixed(2)} no está no peso ideal`;
+            resultado.innerHTML = `Seu IMC é ${resultado.value.toFixed(2)} no está no peso ideal
+                                    <a href="#ilustracao"><img src="assets/seta.svg" alt="" class="seta"></a>`;
             ilustracao.innerHTML = `<img src="assets/img2.png" alt="" class="ilustracao-item">`
             ilustracao.style.display = "flex";
         }
         else if(resultado.value >= 25 && resultado.value <= 29.99){
-            resultado.textContent = `Seu IMC é ${resultado.value.toFixed(2)} e você está com excesso de peso`;
+            resultado.innerHTML = `Seu IMC é ${resultado.value.toFixed(2)} e você está com excesso de peso
+                                    <a href="#ilustracao"><img src="assets/seta.svg" alt="" class="seta"></a>`;
             ilustracao.innerHTML = `<img src="assets/img3.png" alt="" class="ilustracao-item">`
             ilustracao.style.display = "flex";
         }
         else if(resultado.value >= 30 && resultado.value <= 34.99){
-            resultado.textContent = `Seu IMC é ${resultado.value.toFixed(2)} e você está com Obesidade classe I`;
+            resultado.innerHTML = `Seu IMC é ${resultado.value.toFixed(2)} e você está com Obesidade classe I
+                                     <a href="#ilustracao"><img src="assets/seta.svg" alt="" class="seta"></a>`;
             ilustracao.innerHTML = `<img src="assets/img4.png" alt="" class="ilustracao-item">`
             ilustracao.style.display = "flex";
         }
         else if(resultado.value >= 35 && resultado.value <= 39.99){
-            resultado.textContent = `Seu IMC é ${resultado.value.toFixed(2)} e você está com Obesidade classe II`;
+            resultado.innerHTML = `Seu IMC é ${resultado.value.toFixed(2)} e você está com Obesidade classe II
+                                    <a href="#ilustracao"><img src="assets/seta.svg" alt="" class="seta"></a>`;
             ilustracao.innerHTML = `<img src="assets/img5.png" alt="" class="ilustracao-item">`
             ilustracao.style.display = "flex";
         }
         else if(resultado.value >= 40){
-            resultado.textContent = `Seu IMC é ${resultado.value.toFixed(2)} e você está com Obesidade classe III`;
+            resultado.innerHTML = `Seu IMC é ${resultado.value.toFixed(2)} e você está com Obesidade classe III
+                                    <a href="#ilustracao"><img src="assets/seta.svg" alt="" class="seta"></a>`;
             ilustracao.innerHTML = `<img src="assets/img6.png" alt="" class="ilustracao-item">`
             ilustracao.style.display = "flex";
         }
